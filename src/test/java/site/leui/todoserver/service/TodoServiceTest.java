49ef16c1d8dc7ca1f44e8aae106b85a81be1c940
Eui9179 <outdark97@gmail.com>
2023-12-14 22:36:41 +0900

    Test/todo-service-cr

---
+++ src/test/java/site/leui/todoserver/service/TodoServiceTest.java
@@ -0,0 +1,71 @@
+package site.leui.todoserver.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import site.leui.todoserver.domain.Todo;
+import site.leui.todoserver.dto.TodoForSave;
+import site.leui.todoserver.repository.TodoMapper;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+
+public class TodoServiceTest {
+    private final TodoMapper todoRepository = Mockito.mock(TodoMapper.class);
+    private TodoService todoService;
+
+    @BeforeEach
+    public void setUpTest() {
+        todoService = new TodoService(todoRepository);
+    }
+
+    @Test
+    void getTodoTest() {
+        //given
+        Long id = 0L;
+        Todo givenTodo = new Todo(
+                id,
+                "title1",
+                "ready",
+                LocalDateTime.now().plusDays(3),
+                LocalDateTime.now(),
+                null
+        );
+
+        //when
+        Mockito.when(todoRepository.findOne(id))
+                .thenReturn(givenTodo);
+
+        Todo todo = todoService.get(id);
+
+        //then
+        assertEquals(todo.getId(), givenTodo.getId());
+        assertEquals(todo.getTitle(), givenTodo.getTitle());
+        assertEquals(todo.getStatus(), givenTodo.getStatus());
+        assertEquals(todo.getCreatedTime(), givenTodo.getCreatedTime());
+
+        Mockito.verify(todoRepository).findOne(id);
+    }
+
+    @Test
+    void saveTodoTest() {
+        //given
+        Mockito.doNothing()
+                .when(todoRepository)
+                .insert(any(TodoForSave.class));
+
+        // when
+        TodoForSave savedTodo = new TodoForSave();
+        savedTodo.setId(0L);
+        Long id = todoService.save(savedTodo);
+
+        //then
+        assertEquals(savedTodo.getId(), id);
+
+        Mockito.verify(todoRepository).insert(any());
+    }
+}
